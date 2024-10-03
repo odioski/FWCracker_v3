6136371af6273f3241ba71bfb4e085822fc16179
Omar Daniels Sr. <93099372+odioski@users.noreply.github.com>
2024-10-03 09:44:11 -0500

    Update FWCracker.py

--- FWCracker.py
+++ FWCracker.py
@@ -291,7 +291,7 @@ def installer():
             try: # setting (temp) ~\PATH to AppData if got python version
                 
                 set_path = 'set PATH="%PATH%;C:\\~\\AppData\\Roaming\\Python\\' + pyVersion_string + '\\Scripts\"'
-                    # use setx to set permanent addition(s) to PATH
+                    # use setx to set permanent addition(s) to your PATH
 
                 added_path = 'C:\~\AppData\Roaming\Python\\' + pyVersion_string + '\\Scripts\'
 
